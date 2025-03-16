1e82bdf526aad163d94a01a798508c5ed5b75c4d
lavooriganesh <ganeshlavoori@gmail.com>
2025-03-17 01:56:52 +0530

    Update upperlowercases.java

--- upperlowercases.java
+++ upperlowercases.java
@@ -1,16 +1,13 @@
 public class upperlowercases{
     public static void main(String[] args) {
-        char ch = 'A'; // Example input character
-
-        // Check if the character is an uppercase letter
+        char ch = 'A';
         if (ch >= 'A' && ch <= 'Z') {
-            System.out.println(ch + " is an uppercase letter.");
+            System.out.println(ch + " is an uppercase letter");
         }
-        // Check if the character is a lowercase letter
         else if (ch >= 'a' && ch <= 'z') {
-            System.out.println(ch + " is a lowercase letter.");
+            System.out.println(ch + " is a lowercase letter");
         } else {
-            System.out.println(ch + " is not a letter.");
+            System.out.println(ch + " is not a letter");
         }
     }
 }
